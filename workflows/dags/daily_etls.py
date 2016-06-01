36082bd56a171dd2098170a281e1a6015a5f66ec
yasha-podeswa-hs <yasha.podeswa@hootsuite.com>
2016-06-01 00:55:03 -0400

    Organize code

--- workflows/dags/daily_etls.py
+++ workflows/dags/daily_etls.py
@@ -4,37 +4,9 @@ from operators.s3_sensor import S3DatepartSensor
 from airflow import DAG
 
 from datetime import timedelta
-import json
-
-
-# Helper functions
-# ~~~~~~~~~~~~~~~
-
-def context_to_datepart(context):
-    """
-    Creates a partial S3 path based on an Airflow job context
-
-    :param context: a dictionary with details about the job run, where 'tomorrow_ds' is a string holding the day after
-                    the job run, in YYYY-MM-DD format
-    :return: a partial S3 path
-    """
-    return context['tomorrow_ds'].replace('-', '/')
-
-
-def context_to_payload(context):
-    """
-    Creates a payload used to run an AWS Lambda function, based on an Airflow job context
-
-    :param context: a dictionary with details about the job run, where 'ds' is a string holding the day of the job run,
-                    in YYYY-MM-DD format
-    :return: a payload used to run an AWS Lambda function
-    """
-    return json.dumps({'runDate': context['ds']})
 
 
 # DAG
-# ~~~~~~~~~~~~~~~
-
 daily_etls = DAG(
     dag_id='daily_etls',
     schedule_interval=timedelta(days=1),
@@ -45,8 +17,6 @@ daily_etls = DAG(
 
 
 # Nodes
-# ~~~~~~~~~~~~~~~
-
 s3_sensor = S3DatepartSensor(
     task_id='s3_sensor',
     context_to_datepart=context_to_datepart,
@@ -71,7 +41,5 @@ etl_organization_stats = LambdaOperator(
 
 
 # Edges
-# ~~~~~~~~~~~~~~~
-
 etl_load_events.set_upstream(s3_sensor)
 etl_organization_stats.set_upstream(etl_load_events)
--- workflows/settings/job_properties.py
+++ workflows/settings/job_properties.py
@@ -1,6 +1,7 @@
 from airflow.configuration import conf
 
 from datetime import timedelta, datetime
+import json
 
 def days_ago(days):
     now = datetime.now() - timedelta(days=days)
@@ -23,3 +24,24 @@ DEFAULT_ARGS = {
     'retry_delay': timedelta(seconds=10),
     'poke_interval': 30,
 }
+
+def context_to_datepart(context):
+    """
+    Creates a partial S3 path based on an Airflow job context
+
+    :param context: a dictionary with details about the job run, where 'tomorrow_ds' is a string holding the day after
+                    the job run, in YYYY-MM-DD format
+    :return: a partial S3 path
+    """
+    return context['tomorrow_ds'].replace('-', '/')
+
+
+def context_to_payload(context):
+    """
+    Creates a payload used to run an AWS Lambda function, based on an Airflow job context
+
+    :param context: a dictionary with details about the job run, where 'ds' is a string holding the day of the job run,
+                    in YYYY-MM-DD format
+    :return: a payload used to run an AWS Lambda function
+    """
+    return json.dumps({'runDate': context['ds']})
